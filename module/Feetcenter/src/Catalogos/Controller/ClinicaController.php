19ed4407a61a435f856b9eab3bfd7e13582fde7d
Daniel Castanedo Barragan <daniel.b@hostdime.com.mx>
2015-11-09 17:15:14 -0600

    feet

--- module/Feetcenter/src/Catalogos/Controller/ClinicaController.php
+++ module/Feetcenter/src/Catalogos/Controller/ClinicaController.php
@@ -20,8 +20,8 @@ class ClinicaController extends AbstractActionController
             
             $now = new \DateTime();
             $idempleado = $this->params()->fromQuery('idempleado');
-            $result = \VisitaQuery::create()->filterByIdempleado($idempleado)->filterByVisitaFechainicio(array('min' => $now))->exists();
-            
+            $result = \VisitaQuery::create()->filterByIdempleado($idempleado)->filterByVisitaFechainicio(array('min' => $now))->toString();
+            echo '<pre>';var_dump($result); echo '</pre>';exit();
             return $this->getResponse()->setContent(json_encode($result));
         }
     }
